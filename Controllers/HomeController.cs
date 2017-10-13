866d023e988d999fdccb9bd1db494a5f46769dc0
Kimberly Bordon <kbordon@gmail.com>
2017-10-12 20:19:16 -0700

    Update ToDoList app with Category class.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,27 +7,74 @@ namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/")]
+        [HttpGet("/")]
         public ActionResult Index()
         {
-            return View(Task.GetAll());
+            return View();
         }
 
-        [HttpPost("/task/list/add"), ActionName("Index")]
-        public ActionResult IndexPost()
+        [HttpGet("/categories")]
+        public ActionResult Categories()
         {
-            if (Request.Form["new-task"] != "")
-            {
-                Task newTask = new Task (Request.Form["new-task"]);
-            }
-            return View(Task.GetAll());
+            List<Category> allCategories = Category.GetAll();
+            return View(allCategories);
         }
 
-        [HttpPost("/task/list/clear"), ActionName("Index")]
-        public ActionResult TaskListClear()
+        [HttpGet("/categories/new")]
+        public ActionResult CategoryForm()
         {
-            Task.ClearAll();
-            return View(Task.GetAll());
+            return View();
+        }
+
+        [HttpPost("/categories")]
+        public ActionResult AddCategory()
+        {
+            Category newCategory = new Category(Request.Form["category-name"]);
+            List<Category>. allCategories = Category.GetAll();
+            return View("Categories", allCategories);
+        }
+
+        [HttpGet("/categories/{id}")]
+        public ActionResult CategoryDetail(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category selectedCategory = Category.Find(id);
+            List<Task> categoryTasks = selectedCetagory.GetTasks();
+            model.Add("category", selectedCategory);
+            model.Add("tasks", categoryTasks);
+            return View(model);
+        }
+
+        [HttpGet("/categories/{id}/tasks/new")]
+        public ActionResult CategoryTaskForm(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category selectedCategory = Category.Find(id);
+            List<Task> allTasks = selectedCategory.GetTasks();
+            model.Add("category", selectedCategory);
+            modelAdd("tasks", allTasks);
+            return View(model);
+        }
+
+        [HttpPost("/tasks")]
+        public ActionResult AddTask()
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category selectedCategory = Category.Find(Int32.Parse(Request.Form["category-id"]));
+            List<Task> categoryTasks = selectedCategory.GetTasks();
+            string taskDescription = Request.Form["task-description"];
+            Task newTask = new Task(taskDescription);
+            categoryTasks.Add(newTask);
+            model.Add("tasks", categoryTasks);
+            model.Add("category", selectedCategory);
+            return View("CategoryDetail", model);
+        }
+
+        [HttpGet("/tasks/{id}")]
+        public ActionResult TaskDetail(int id)
+        {
+            Task task = Task.Find(id);
+            return View(task);
         }
     }
 }
